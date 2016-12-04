b0626bc39086d710854c7f66afd0f9c57b18bea1
hungppse03862 <hungppse03862@fpt.edu.vn>
2016-12-04 16:00:09 +0700

    add project

---
+++ Board/ToaDo.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Board
+{
+    class ToaDo
+    {
+        public int x;
+        public int y;
+
+        public ToaDo(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
